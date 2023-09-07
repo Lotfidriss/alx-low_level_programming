052bd2d02219c0ca93c7cdff631fcfb00c4c4360
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-07 14:16:48 -0700

    printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
+#include <unistd>
 
 /**
  * main - this is the main
  *
- * REturn: 0 on success
+ * Return: Always 0 on success
  */
-int main (void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
