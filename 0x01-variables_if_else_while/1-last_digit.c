421c8580b1d531e2803af9416a66c51d5fbbd1cd
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-08 12:02:53 -0700

    any

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
 	if (digit > 5)
-		print("Last digit of %i is %i and is greater than 5\n", n, digit)
+		print("Last digit of %i is %i and is greater than 5\n", n, digit);
 	else if (digit == 0)
-		print("Last digit of %i is %i andis 0\n", n, digit)
+		print("Last digit of %i is %i andis 0\n", n, digit);
 	else if (digit < 6 && digit != 0)
-		print("Last digit of %i is %i and is less than 6 and not 0", n, digit)
+		print("Last digit of %i is %i and is less than 6 and not 0", n, digit);
 
 	return (0);
 }
