96d0d8529f98d397d154a33b75e4b30042db1427
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-08 16:44:28 -0700

    any

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ *
+ * Description: print the letter lowrcase
+ *
+ * Return: 0 (seccess)
+ */
+
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' || letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
