8cebb276c852d196aae9d153fd946da2fd22a5c1
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-08 17:00:26 -0700

    any

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ *
+ * Description: print the letter lowrcase
+ *
+ * Return: 0 (seccess)
+ */
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+}
