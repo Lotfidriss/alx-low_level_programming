8509c8f4c54d7500d972b70670a61413e2bcd6d4
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-08 17:10:43 -0700

    any

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ *
+ * Description: print the letter lowrcase
+ *
+ * Return: 0 (seccess)
+ */
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
