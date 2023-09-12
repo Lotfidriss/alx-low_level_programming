881ea0a6fe13cbfc94c2abe1272aaafaad2bdcd8
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-12 12:09:28 -0700

    any

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include"main.h"
 
 /**
- *
  * print_alphabet - utiler _putchar
- *
  */
 
 void print_alphabet(void)
 
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
