b76ee6a5eff4387cc24c2b79fa95d744cead0009
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-12 12:42:48 -0700

    any

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,18 +4,18 @@
  * print_alphabetx100 - utiler _putchar
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 
 {
 	int line, ch;
 
 	for (line = 0; line <= 9; line++)
 	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
 		_putchar(ch);
 	
-	}
+		}
 	_putchar('\n');
 	}
 }
