a225d95ce11da05fb82405cf3ac3468fd2b8d5ed
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-12 18:31:46 -0700

    any

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 			 * put space if product is simple number
 			 * place the frist digit if its two numbers
 			 */
-			if ( prod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48); /*get the first digit*/
