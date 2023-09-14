39598838326c85ad3b5b13d4397c8935623064fa
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 16:01:26 -0700

    any

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,10 @@
 #include"main.h"
 
 /**
- *
  * mul - function function that multiplies two integers
- *
  * @a : first input
  * @b : second input
- *
  * Return: results
- *
- * description :  function check for upper
  */
 int mul(int a, int b)
 {
