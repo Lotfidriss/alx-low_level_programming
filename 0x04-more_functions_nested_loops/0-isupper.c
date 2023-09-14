ff520768bbe2a8ec75955bd8e49a2c970e85890c
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 11:14:49 -0700

    any

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,13 @@
  *
  * Return: (1) if c is uppercase otherwise always (0)
  *
- * decription : function check for upper
+ * description :  function check for upper
  */
 
 int _usupper(int c)
 {
-	int c;
 
-	if (c >= 655555c <= 90)
+	if (c >= 45 && <= 90)
 		return (1);
 		return (0);
 }
