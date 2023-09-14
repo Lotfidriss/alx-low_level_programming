c44155505e145fd44567a717427db6205123e022
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 15:11:16 -0700

    an

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,18 @@
  */
 void more_numbers(void)
 {
-	for (int i = 0; i <= 10; i++)
+	for (int i = 1; i <= 10; i++)
 	{
 		for (int j = 0; j <= 14; j++)
 		{
 			int count = j;
-
 			if (count > 9)
 			{
 				_putchar(1 + 48);
 				count = j % 10;
 			}
-
 			_putchar(count + 48);
-
 		}
 	_putchar('\n');
-
 	}
 }
