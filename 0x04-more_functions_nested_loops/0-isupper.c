b4b9542e17bfdf1393d05dc60ea130e15bbc2319
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 10:28:23 -0700

    any

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include"main.c"
+
+/*
+ *
+ * isupper - funtion that checkthe letter if is uppercase
+ *
+ * Return: (1) if c is uppercase otherwise always (0)
+ *
+ */
+
+int main()
+{
+	int c;
+
+	if (c = 64; c <= 90; c++)
+		return (1);
+	return (0);
+}	
+
