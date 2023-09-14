ec199e24a5204e1a3378ce2a2f5f17ab6fdc168d
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 16:29:48 -0700

    any

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i, j, count;
+	int i, j;
 	for (i = 1; i <= 10; i++)
 	{
 	for (j = 0; j <= 14; j++)
 	{
-	int count = j;
-	if (count > 9)
+	if (j > 9)
 	{
-	_putchar(1 + 48);
-	count = j % 10;
+	_putchar((j / 10) + '0');
 	}
-	_putchar(count + 48);
+	_putchar((j % 10)+ '0');
 	}
 	_putchar('\n');
 	}
