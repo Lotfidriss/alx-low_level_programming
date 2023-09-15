a9fa914a545d3570266e60cfe3ddff86b56184b2
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 17:05:15 -0700

    any

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws strainght according
+ * @n: number of line to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	if (i <= 0)
+	{
+	_putchar('\n);
+	}
+	else
+	{
+	for (i = 0; i < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
