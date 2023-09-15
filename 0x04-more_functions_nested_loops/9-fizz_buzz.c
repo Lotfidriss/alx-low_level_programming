ba2e9fd242f96bde2c9a3e7e2cade1afb07911a2
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-14 17:42:10 -0700

    any

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - print a fizz buzz prograam
+ * Return: always 0
+ */
+
+int main(vois)
+{
+
+	int i;
+	for (i = 1; i <= 100; i++)
+	{	
+	if ((i % 3 == 0) && (i % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (i % 3 ==0)
+	{
+	printf("Fizz");
+	}	
+	else if (i % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else 
+	{
+	printf("%d", i);
+	}
+	if (i != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
