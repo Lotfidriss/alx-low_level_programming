20ea4c518f2325da674fdec1f12432f799d084b4
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-18 08:47:07 -0700

    any

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,10 @@
 /**
  * reset_to_98 -  parameter and updates the value it points to to 98
  * Return: always (0)
+ * @n : number 98
 */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	int n;
 	int *p = &n;
