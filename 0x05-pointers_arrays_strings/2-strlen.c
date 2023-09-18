d782580a1408677bd16f7f55b6e6d86127230565
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-18 15:21:12 -0700

    any

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - count letters
+ * @s: count nulbers of char
+ * Return: len
+ */
+	int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+
+}
