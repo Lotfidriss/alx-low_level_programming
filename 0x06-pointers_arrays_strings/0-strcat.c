4c8ff918933209034fb6c423838d2d95410f72db
Lotfidriss <driss.lotfi9@gmail.com>
2023-09-21 08:14:34 -0700

    any

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include"main.h"
 /**
- * _strcat - function that concatenates two strings.
+ * _strcat - concatenates two strings
  * @dest : variable type char
  * @src : variable type char
- */
+ * Return: void
+*/
 
 
 char *_strcat(char *dest, char *src)
